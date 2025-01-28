e69e8d3d353d272da9e136606dcb181b8088bce1
AbdAlhadi Jouda <abdelhad@smartpace.co.uk>
2025-01-28 18:58:42 +0200

    Implement ShouldQueue interface

--- app/Notifications/V1/AutoUserPasswordResetNotification.php
+++ app/Notifications/V1/AutoUserPasswordResetNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class AutoUserPasswordResetNotification extends Notification implements ShouldBroadcast
+class AutoUserPasswordResetNotification extends Notification implements ShouldBroadcast, ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/V1/UserRoleUpdateNotification.php
+++ app/Notifications/V1/UserRoleUpdateNotification.php
@@ -5,11 +5,10 @@ namespace App\Notifications\V1;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\BroadcastMessage;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserRoleUpdateNotification extends Notification implements ShouldBroadcast
+class UserRoleUpdateNotification extends Notification implements ShouldBroadcast, ShouldQueue
 {
     use Queueable;
 
@@ -28,7 +27,7 @@ class UserRoleUpdateNotification extends Notification implements ShouldBroadcast
      */
     public function via(object $notifiable): array
     {
-        return ['database','broadcast'];
+        return ['database', 'broadcast'];
     }
 
     /**
@@ -37,9 +36,9 @@ class UserRoleUpdateNotification extends Notification implements ShouldBroadcast
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
     }
 
     /**

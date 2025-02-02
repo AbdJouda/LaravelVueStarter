d0cb286fe235b1d13833056e67e2645136466a9a
AbdAlhadi Jouda <abdelhad@smartpace.co.uk>
2025-02-02 12:28:54 +0200

    Refactor & Optimize

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
-import App from './App.vue'
+import App from './App.vue';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -7,24 +7,15 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import router from './router';
-import vue3GoogleLogin from 'vue3-google-login'
+import vue3GoogleLogin from 'vue3-google-login';
 import VueAppleLogin from 'vue-apple-login';
 import { useAuthStore } from '@/stores/authStore';
 import { useSettingsStore } from '@/stores/settingsStore';
 import { hasPermissionDirective } from '@/plugins/permissionDirectives';
 
+library.add(fas, fab);
 
-const createNewApp = async () => {
-    const app = createApp({
-        render: () => h(App),
-    })
-    library.add(fas, fab)
-    app.component('font-awesome-icon', FontAwesomeIcon)
-    app.use(router)
-    app.use(ElementPlus)
-    app.use(createPinia())
-    app.directive('hasPermission', hasPermissionDirective);
-
+const initializeStores = async () => {
     const authStore = useAuthStore();
     await authStore.initializeAuthentication();
 
@@ -32,10 +23,25 @@ const createNewApp = async () => {
     if (!settingsStore.isLoaded) {
         await settingsStore.fetchSettings();
     }
+};
+
+const createNewApp = async () => {
+    const app = createApp({
+        render: () => h(App),
+    });
+
+    app.component('font-awesome-icon', FontAwesomeIcon);
+
+    app.use(router);
+    app.use(ElementPlus);
+    app.use(createPinia());
+    app.directive('hasPermission', hasPermissionDirective);
+
+    await initializeStores();
 
     app.use(vue3GoogleLogin, {
-        clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID
-    })
+        clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    });
 
     app.use(VueAppleLogin, {
         clientId: import.meta.env.VITE_SIGN_IN_WITH_APPLE_CLIENT_ID,
@@ -45,13 +51,9 @@ const createNewApp = async () => {
         usePopup: true,
     });
 
-        app.mount('#app')
-}
+    app.mount('#app');
+};
 
-const initApp = async () => {
-    createNewApp()
-}
-
-initApp().then(() => {
-    // initialized
-})
+createNewApp().catch((error) => {
+    console.error('Failed to initialize the app:', error);
+});

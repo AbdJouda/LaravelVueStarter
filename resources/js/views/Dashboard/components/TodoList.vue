38414eaf9c87b4e8cb9ead4b98db6b73a0e32eaf
AbdAlhadi Jouda <abdelhad@smartpace.co.uk>
2025-02-06 20:01:47 +0200

    Bug Fix

--- resources/js/views/Dashboard/components/TodoList.vue
+++ resources/js/views/Dashboard/components/TodoList.vue
@@ -1,25 +1,3 @@
-<template>
-    <el-card class="shadow-lg rounded-lg">
-        <!-- Card Header -->
-        <div class="flex justify-between items-center border-b pb-4 mb-4">
-            <h3 class="font-semibold">Upcoming Tasks</h3>
-            <el-button type="primary" size="small" @click="router.push({ name: RouteNames.TODO_LIST })">View All</el-button>
-        </div>
-
-        <!-- Todo List -->
-        <div  class="space-y-4">
-            <div v-if="todos.length === 0"
-                 class="flex items-center dark:bg-gray-800 dark:border-gray-700 justify-between p-4 bg-white rounded-lg shadow-sm border border-gray-200">
-                No todos available. Add a new todo to get started!
-            </div>
-            <TodoItem v-else v-for="(item, index) in todos"
-                      :key="index"
-                      :item="item" />
-        </div>
-
-    </el-card>
-</template>
-
 <script setup>
 import { useTodosStore } from '@/stores/todosStore';
 import TodoItem from "@/views/Todo/components/TodoItem.vue";
@@ -33,7 +11,27 @@ onMounted(async () => {
     await todoStore.fetchUpcomingTodos();
 });
 </script>
+<template>
+    <el-card class="shadow-lg rounded-lg">
+        <!-- Card Header -->
+        <div class="flex justify-between items-center border-b pb-4 mb-4">
+            <h3 class="font-semibold">Upcoming Tasks</h3>
+            <el-button type="primary" size="small" @click="router.push({ name: RouteNames.TODO_LIST })">View All</el-button>
+        </div>
+
+        <!-- Todo List -->
+        <div  class="space-y-4">
+            <div v-if="todos?.length === 0"
+                 class="flex items-center dark:bg-gray-800 dark:border-gray-700 justify-between p-4 bg-white rounded-lg shadow-sm border border-gray-200">
+                No todos available. Add a new todo to get started!
+            </div>
+            <TodoItem v-else v-for="(item, index) in todos"
+                      :key="index"
+                      :item="item" />
+        </div>
+
+    </el-card>
+</template>
 
 <style scoped>
-/* Custom styles for additional tweaks */
 </style>
